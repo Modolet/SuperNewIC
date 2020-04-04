cce64a2c9cd23648d0b76ef48e9023791d997d87
modolet <modolet@163.com>
2020-04-04 19:30:13 +0800

    添加了教师端程序

---
+++ SNITeacher/mainwindow.h
@@ -0,0 +1,40 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class QIcon;
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_list_clicked();
+    void on_pushButton_notice_clicked();
+    void on_pushButton_status_clicked();
+    void on_pushButton_message_clicked();
+    void on_pushButton_score_clicked();
+    void on_style1_triggered();
+    void on_style2_triggered();
+    void on_style3_triggered();
+    void on_style4_triggered();
+    void on_style5_triggered();
+    void on_style6_triggered();
+
+private:
+    QIcon* p_icon;
+    Ui::MainWindow *ui;
+    void initWindow();
+    void initQss(int n);
+    void initIcons();
+};
+#endif // MAINWINDOW_H
